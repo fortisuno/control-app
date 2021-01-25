5d58d82fdfaa71a1fe43661fbc3b23e5e77e4480
Pablo Gomez <pgomezm1400@alumno.ipn.mx>
2021-01-25 00:17:27 -0600

    fetch data

--- pages/[view].js
+++ pages/[view].js
@@ -2,7 +2,7 @@ import Content from '@components/content';
 import Nav from '@components/nav';
 import UserContext from '@components/user-context';
 import DashboardLayout from '@layouts/dashboard-layout'
-import React, {useContext, useEffect} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 
 export async function getStaticPaths() {
   return {
@@ -35,6 +35,21 @@ export async function getStaticProps({params}) {
 export default function View({view, menu}) {
   const title = view.charAt(0).toUpperCase() + view.slice(1);
   const { user } = useContext(UserContext) // puedes acceder al nombre del usuario con user.id y al token con user.token
+  const [registros, setRegistros] = useState(null)   
+
+  useEffect(() => {
+    // Aqui haz la consulta
+    //getRegistros().then(resp => {
+    //  if (resp.ok) {
+    //    setRegistros(resp.data) // el resp.data es el array de la respuesta
+    //  } else {
+          setTimeout(() => {
+            setRegistros([])
+          }, 1000)
+    //  }
+    //}
+    //})
+  }, [])
 
   if (user.id == null) {
     return <span>Loading...</span>
@@ -49,7 +64,42 @@ export default function View({view, menu}) {
       <h1 className="mb-4">{title}</h1>
       <Nav menu={menu}>
         <Content id="tab-consulta" defaultView>
-          <p>Contenido de Consultas</p>
+          <ul className="list-group">
+            <li className="list-group-item active">
+              <div className="row text-center">
+                <span className="col">Campo1</span>
+                <span className="col">Campo2</span>
+                <span className="col">Campo3</span>
+                <span className="col">Campo4</span>
+              </div>
+            </li>
+            {
+              registros != null
+                ? registros.length > 0
+                    ? (
+                        <li className="list-group-item">
+                          <div className="row text-center">
+                            <span className="col">Campo1</span>
+                            <span className="col">Campo2</span>
+                            <span className="col">Campo3</span>
+                            <span className="col">Campo4</span>
+                          </div>
+                        </li>
+                      )
+                    : (
+                        <li className="list-group-item text-center py-5">
+                          No hay resultados
+                        </li>
+                      )
+                : (
+                    <li className="list-group-item text-center py-5">
+                      <div className="spinner-border text-primary" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                      </div>
+                    </li>
+                  )
+            }
+          </ul>
         </Content>
         <Content id="tab-alta">
           <p>Contenido de altas</p>
